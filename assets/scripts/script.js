15a67223f4155724e50ed0e2f25f2995cbf0b543
Viniccius Wirz <vinimovel23@gmail.com>
2023-01-17 19:14:09 -0300

    Valida login pela API

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,32 @@
 const server = 'https://mock-api.driven.com.br/api/v6/uol/participants';
 let sessionName = '';
 let question = 'Insira seu nome de login:';
-// login();
-function login(){
+
+
+login();
+function login() {
     const nameRequest = prompt(question);
-    const name = {name:nameRequest};
-    const promise = axios.post(server, name);
-    promise.then(success);
-    promise.catch(failed);
+    if (nameRequest !== '') {
+        const name = { name: nameRequest };
+        const promise = axios.post(server, name);
+        promise.then(success);
+        promise.catch(failed);
+    }
 }
 
-function failed(){
-    question = 'Este nome já está em uso, insira outro nome';
-    login();
+function success(answer) {
+    const serverStatus = answer.status;
+    if (serverStatus === 200) {
+        sessionName = JSON.parse(answer.config.data).name;
+    }
 }
 
-function success(answer){
-    sessionName = JSON.parse(answer.config.data).name;
-    console.log(answer)
+function failed(answer) {
+    const serverStatus = answer.response.status;
+    if (serverStatus === 400) {
+        alert('Este nome de usuário já está em uso, Por favor escolha outro');
+        window.location.reload();
+    } else {
+        alert('Erro de conexão');
+    }
 }
